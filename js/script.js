33228d209651e1d22191199feccf50e197e69e15
kazblacktopp <karen.blacktopp@gmail.com>
2021-09-06 16:15:59 +1000

    Corrected some syntax in js code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
 //////////////////////////////////////////////////
 // Change copyright date in Footer to current year
 
-const yearEl = document.querySelector(".year");
+const yearEl = document.querySelector('.year');
 
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
@@ -11,59 +11,59 @@ yearEl.textContent = currentYear;
 //////////////////////////////////////////////////
 // Animate mobile navigation
 
-const sectionNavEl = document.querySelector(".section-navigation");
-const mobileNavBtnEl = document.querySelector(".nav-btn-mobile");
+const sectionNavEl = document.querySelector('.section-navigation');
+const mobileNavBtnEl = document.querySelector('.nav-btn-mobile');
 
-mobileNavBtnEl.addEventListener("click", () => {
-  sectionNavEl.classList.toggle("nav-open");
+mobileNavBtnEl.addEventListener('click', () => {
+  sectionNavEl.classList.toggle('nav-open');
 });
 
 //////////////////////////////////////////////////
 // Create smooth scroll effect for all browsers
 
-const allLinksEl = document.querySelectorAll("a:link");
+const allLinksEl = document.querySelectorAll('a:link');
 
-allLinksEl.forEach((link) => {
-  link.addEventListener("click", (e) => {
+allLinksEl.forEach(link => {
+  link.addEventListener('click', e => {
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
 
     // Scroll back to top
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     }
 
     // Close mobile navigation when link clicked
-    if (link.classList.contains("main-nav-link"))
-      sectionNavEl.classList.toggle("nav-open");
+    if (link.classList.contains('main-nav-link'))
+      sectionNavEl.classList.toggle('nav-open');
   });
 });
 
 //////////////////////////////////////////////////
 // Create sticky navigation for all browsers
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector('.section-hero');
 
 const obs = new IntersectionObserver(
-  (entries) => {
+  entries => {
     const ent = entries[0];
-    if (!ent.isIntersecting) document.body.classList.add("js-sticky");
-    if (ent.isIntersecting) document.body.classList.remove("js-sticky");
+    if (!ent.isIntersecting) document.body.classList.add('js-sticky');
+    if (ent.isIntersecting) document.body.classList.remove('js-sticky');
   },
   {
     root: null,
     threshold: 0,
-    rootMargin: "-64px",
+    rootMargin: '-64px',
   }
 );
 obs.observe(sectionHeroEl);
@@ -73,14 +73,14 @@ obs.observe(sectionHeroEl);
 
 const checkFlexGap = () => {
   // Create flex container with row-gap set
-  const flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  const flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
   // Create two elements inside the container
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   // Append container to DOM IOT read scrollHeight
   document.body.appendChild(flex);
@@ -91,5 +91,5 @@ const checkFlexGap = () => {
 };
 
 if (!checkFlexGap()) {
-  document.body.classList.add("no-flex-gap");
+  document.body.classList.add('no-flex-gap');
 }
